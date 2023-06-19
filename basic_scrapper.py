1687bf985a02fa66ceb86a833743143e3e0bfc22
siddharthraja9849 <siddharth.raja@dunzo.in>
2023-06-20 02:20:24 +0530

    add basic JobKeys and BaseJobClass

--- basic_scrapper.py
+++ basic_scrapper.py
@@ -7,7 +7,32 @@ from bs4 import BeautifulSoup
 url='https://www.cermati.com/karir/lowongan'
 driver=webdriver.Chrome()
 driver.get(url)
-      
+
+class JobKeys:
+    """
+        Basic job keys
+    """
+    LOCATION = "location"
+    TITLE = "title"
+    SUBTITLE = "subtitle"
+    JOB_TYPE = "job_type"
+    URL = "url"
+
+
+class BaseJobClass:
+    """
+        Basic Job Class
+    """
+    def __init__(self, kwargs):
+        self.title = kwargs.get(JobKeys.TITLE)
+        self.subtitle = kwargs.get(JobKeys.SUBTITLE)
+        self.url = kwargs.get(JobKeys.URL)
+        self.location = kwargs.get(JobKeys.LOCATION)
+        self.job_type = kwargs.get(JobKeys.JOB_TYPE)
+    
+    def get_json(self):
+        return self.__dict__
+
 def getDetails(name):
     """
         Fetches location and department keys
@@ -20,25 +45,37 @@ def getDetails(name):
 
 def gatherJobListWrapper(wrapperClass="page-job-list-wrapper"):
     
+
     for wrapper in driver.find_elements(By.CLASS_NAME, wrapperClass):
 
+        jobDic = {}
         for jobDetailHtml in wrapper.find_elements(By.CLASS_NAME, "job-detail"):
-
+            
             if jobDetailHtml.tag_name == "div":
                 
                 if "jobs-location-wrapper" in jobDetailHtml.get_dom_attribute("class"):
-                    print("Job location: ",jobDetailHtml.text, end=" ")
+                    #print("Job location: ",jobDetailHtml.text, end=" ")
+                    jobDic[JobKeys.LOCATION] = jobDetailHtml.text
                 else :
                     jobTitleHtml = jobDetailHtml.find_element(By.TAG_NAME, "strong")
                     jobSubtitleHtml = jobDetailHtml.find_element(By.TAG_NAME, "p")
-                    print("JobTitle : ", jobTitleHtml.text,end=" ")
-                    print("Job subtitle : ", jobSubtitleHtml.text,end=" ")
+                    #print("JobTitle : ", jobTitleHtml.text,end=" ")
+                    #print("Job subtitle : ", jobSubtitleHtml.text,end=" ")
+                    jobDic[JobKeys.TITLE] = jobTitleHtml.text
+                    jobDic[JobKeys.SUBTITLE] = jobSubtitleHtml.text
             elif jobDetailHtml.tag_name == "p":
-                print("Job Type: ", jobDetailHtml.text,end=" ")
+                #print("Job Type: ", jobDetailHtml.text,end=" ")
+                jobDic[JobKeys.JOB_TYPE] = jobDetailHtml.text
 
         for applyTagLinkHtml in wrapper.find_elements(By.TAG_NAME, "a"):
-            print("Href : ",applyTagLinkHtml.get_attribute("href"),end=" ")
-        
+            #print("Href : ",applyTagLinkHtml.get_attribute("href"),end=" ")
+            jobDic[JobKeys.URL] = applyTagLinkHtml.get_attribute("href")
+            # We need multi threading to achieve this
+            # applyTagLinkHtml.click()
+
+        job = BaseJobClass(jobDic)
+
+        print(job.get_json())
         print()
 
 
@@ -62,10 +99,10 @@ def clicksOnDepartmentnLocation(department="jobDepartment", location="jobLocatio
 
 def getPage():
       
-    departments = getDetails(name="jobDepartment")    
-    locations = getDetails(name="jobLocation") 
-    print(departments)
-    print(locations)
+    #departments = getDetails(name="jobDepartment")    
+    #locations = getDetails(name="jobLocation") 
+    #print(departments)
+    #print(locations)
 
     clicksOnDepartmentnLocation()
 
