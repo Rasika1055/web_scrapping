28c640d2a67f9d9adcb53944ac6043124c51b074
siddharthraja9849 <siddharth.raja@dunzo.in>
2023-06-20 11:53:37 +0530

    add HtmlKeys

--- basic_scrapper.py
+++ basic_scrapper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from dtos.job_keys import JobKeys
+from dtos.job_keys import JobKeys, HtmlKeys
 from dtos.base_job import BaseJobClass
 
 url='https://www.cermati.com/karir/lowongan'
@@ -16,37 +16,37 @@ def getDetails(name):
     """
     arr =[]
     for each in driver.find_elements(By.NAME, name):
-        for htm in each.find_elements(By.TAG_NAME,'option'):
+        for htm in each.find_elements(By.TAG_NAME,HtmlKeys.OPTION_TAG):
             arr.append(htm.text)
     return arr
 
-def gatherJobListWrapper(wrapperClass="page-job-list-wrapper"):
+def gatherJobListWrapper(wrapperClass=HtmlKeys.PAGE_JOB_LIST_WRAPPER):
     
 
     for wrapper in driver.find_elements(By.CLASS_NAME, wrapperClass):
 
         jobDic = {}
-        for jobDetailHtml in wrapper.find_elements(By.CLASS_NAME, "job-detail"):
+        for jobDetailHtml in wrapper.find_elements(By.CLASS_NAME, HtmlKeys.JOB_DETAIL):
             
-            if jobDetailHtml.tag_name == "div":
+            if jobDetailHtml.tag_name == HtmlKeys.DIV_TAG:
                 
-                if "jobs-location-wrapper" in jobDetailHtml.get_dom_attribute("class"):
+                if HtmlKeys.JOBS_LOCATION_WRAPPER in jobDetailHtml.get_dom_attribute(HtmlKeys.CLASS_ATTR):
                     #print("Job location: ",jobDetailHtml.text, end=" ")
                     jobDic[JobKeys.LOCATION] = jobDetailHtml.text
                 else :
-                    jobTitleHtml = jobDetailHtml.find_element(By.TAG_NAME, "strong")
-                    jobSubtitleHtml = jobDetailHtml.find_element(By.TAG_NAME, "p")
+                    jobTitleHtml = jobDetailHtml.find_element(By.TAG_NAME, HtmlKeys.STRONG_TAG)
+                    jobSubtitleHtml = jobDetailHtml.find_element(By.TAG_NAME, HtmlKeys.P_TAG)
                     #print("JobTitle : ", jobTitleHtml.text,end=" ")
                     #print("Job subtitle : ", jobSubtitleHtml.text,end=" ")
                     jobDic[JobKeys.TITLE] = jobTitleHtml.text
                     jobDic[JobKeys.SUBTITLE] = jobSubtitleHtml.text
-            elif jobDetailHtml.tag_name == "p":
+            elif jobDetailHtml.tag_name == HtmlKeys.P_TAG:
                 #print("Job Type: ", jobDetailHtml.text,end=" ")
                 jobDic[JobKeys.JOB_TYPE] = jobDetailHtml.text
 
-        for applyTagLinkHtml in wrapper.find_elements(By.TAG_NAME, "a"):
-            #print("Href : ",applyTagLinkHtml.get_attribute("href"),end=" ")
-            jobDic[JobKeys.URL] = applyTagLinkHtml.get_attribute("href")
+        for applyTagLinkHtml in wrapper.find_elements(By.TAG_NAME, HtmlKeys.A_TAG):
+            #print("Href : ",applyTagLinkHtml.get_attribute(HtmlKeys.HREF_ATTR),end=" ")
+            jobDic[JobKeys.URL] = applyTagLinkHtml.get_attribute(HtmlKeys.HREF_ATTR)
             # We need multi threading to achieve this
             # applyTagLinkHtml.click()
 
@@ -55,16 +55,16 @@ def gatherJobListWrapper(wrapperClass="page-job-list-wrapper"):
         print(job.get_json())
         print()
 
-def clicksOnDepartmentnLocation(department="jobDepartment", location="jobLocation"):
+def clicksOnDepartmentnLocation(department=HtmlKeys.JOB_DEPARTMENT, location=HtmlKeys.JOB_LOCATION):
     """
         Clicks department and location
     """
 
     for eachDepartmentSelectTag in driver.find_elements(By.NAME, department):
-        for deptOption in eachDepartmentSelectTag.find_elements(By.TAG_NAME,'option'):
+        for deptOption in eachDepartmentSelectTag.find_elements(By.TAG_NAME,HtmlKeys.OPTION_TAG):
             for eachLocationSelectTag in driver.find_elements(By.NAME, location):
-                for locOption in eachLocationSelectTag.find_elements(By.TAG_NAME,'option'):
-                    if deptOption != JobKeys.ALL_DEPARTMENT or locOption != JobKeys.ALL_LOCATION:
+                for locOption in eachLocationSelectTag.find_elements(By.TAG_NAME,HtmlKeys.OPTION_TAG):
+                    if deptOption != JobKeys.ALL_DEPARTMENT and locOption != JobKeys.ALL_LOCATION:
                         deptOption.click()
                         locOption.click()
                         gatherJobListWrapper()
--- dtos/job_keys.py
+++ dtos/job_keys.py
@@ -13,3 +13,20 @@ class JobKeys:
     URL = "url"
     ALL_DEPARTMENT =  "All Department"
     ALL_LOCATION = "All Location"
+
+class HtmlKeys:
+    """
+        Html class, id, css selector and tag keys
+    """
+    PAGE_JOB_LIST_WRAPPER = "page-job-list-wrapper"
+    JOBS_LOCATION_WRAPPER = "jobs-location-wrapper"
+    JOB_DETAIL = "job-detail"
+    JOB_DEPARTMENT = "jobDepartment"
+    JOB_LOCATION = "jobLocation"
+    DIV_TAG = "div"
+    CLASS_ATTR = "class"
+    STRONG_TAG = "strong"
+    P_TAG = "p"
+    A_TAG = "a"
+    HREF_ATTR = "href"
+    OPTION_TAG = "option"
